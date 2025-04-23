76eef4688bcfa02766a2c4591679fb89e26ebb14
Renato <idkraes17@gmail.com>
2025-04-22 20:42:02 -0400

    avances navbar

--- src/components/layout/MobileHeader.tsx
+++ src/components/layout/MobileHeader.tsx
@@ -1,6 +1,8 @@
 import logo from "../../assets/images/logo.png";
 import home from "../../assets/icons/home.svg";
 import book from "../../assets/icons/book.svg";
+import promo from "../../assets/icons/promo.svg";
+import phone from "../../assets/icons/phone.svg";
 
 const MobileHeader = () => {
   const navLinks = [
@@ -14,16 +16,26 @@ const MobileHeader = () => {
       href: "#",
       icon: book,
     },
+    {
+      label: "Promo",
+      href: "#",
+      icon: promo,
+    },
+    {
+      label: "Contacto",
+      href: "#",
+      icon: phone,
+    },
   ];
 
   return (
-    <header className="bg-dark-details fixed w-full h-20 bottom-0 flex flex-col items-center">
-      <img className="w-40 h-auto absolute bottom-13" src={logo} alt="" />
-      <ul className="flex gap-5 justify-center items-end h-full pb-5 font-semibold">
+    <header className="bg-dark-details fixed w-full h-28 bottom-0 flex flex-col items-center">
+      <img className="w-40 h-auto absolute bottom-20" src={logo} alt="" />
+      <ul className="flex gap-8 justify-center items-end h-full pb-4 font-semibold text-lg">
         {navLinks.map((el, i) => (
           <li key={i}>
-            <img src={el.icon} alt="" />
-            <a className="flex flex-col items-center" href={el.href}>
+            <a className="flex flex-col items-center text-white hover:text-gray-300" href={el.href}>
+              <img className="w-7" src={el.icon} alt="" />
               {el.label}
             </a>
           </li>
